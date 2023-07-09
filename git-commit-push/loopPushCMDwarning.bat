c5641d8c4d8408e3345d39dfaf611669d909a118
Himanshu-Lilhore <himanshulilhore@gmail.com>
2023-07-09 06:37:05 +0530

    Commit at 2023-07-09 06:37:05

--- git-commit-push/loopPushCMDwarning.bat
+++ git-commit-push/loopPushCMDwarning.bat
@@ -1,11 +1,12 @@
 @echo off
 set /a count=0
 :loop
-timeout /t 10
 call loopPush.bat
+timeout /t 10
 set /a count+=1
 if %count%==2 goto endloop
 goto loop
 :endloop
 color 4F
 echo Warning: The script is about to end. Please restart it if you want to continue monitoring the repository for changes.
+timeout /t 15
