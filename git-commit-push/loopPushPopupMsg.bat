100305f52ae140360fd55570ec01a7127cafc575
Himanshu-Lilhore <himanshulilhore@gmail.com>
2023-07-09 07:09:21 +0530

    Commit at 2023-07-09 07:09:21

--- git-commit-push/loopPushPopupMsg.bat
+++ git-commit-push/loopPushPopupMsg.bat
@@ -1,10 +1,13 @@
+echo "runsFor4hrs"
+
 @echo off
-set /a count=0
+set timeoutDuration=30
+set /a endTime=%time:~0,2%+4
+if %endTime% GTR 23 set /a endTime-=24
 :loop
 call loopPush.bat
-timeout /t 10
-set /a count+=1
-if %count%==2 goto endloop
+timeout /t %timeoutDuration%
+if %time:~0,2% GEQ %endTime% goto endloop
 goto loop
 :endloop
 msg * "Warning: The script has ended. Please restart it if you want to continue monitoring the repository for changes."
