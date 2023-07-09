a0b9159c0adcbec873081a12d1edd0851cab3a65
Himanshu-Lilhore <himanshulilhore@gmail.com>
2023-07-09 06:48:34 +0530

    Commit at 2023-07-09 06:48:33

--- git-commit-push/loopPushPopupMsg.bat
+++ git-commit-push/loopPushPopupMsg.bat
@@ -7,4 +7,4 @@ set /a count+=1
 if %count%==2 goto endloop
 goto loop
 :endloop
-msg * "Warning: The script is about to end. Please restart it if you want to continue monitoring the repository for changes."
+start cmd /k "color 4F & echo ┌────────────────────────────────────────────┐ & echo │                                            │ & echo │ Warning: The script has ended.             │ & echo │ Please restart it if you want to continue  │ & echo │ monitoring the repository for changes.     │ & echo │                                            │ & echo └────────────────────────────────────────────┘"
