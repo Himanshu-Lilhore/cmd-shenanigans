50c5baef70494e71c4f6185921cc8d511b4613af
Himanshu-Lilhore <himanshulilhore@gmail.com>
2023-07-09 07:04:16 +0530

    Commit at 2023-07-09 07:04:16

--- git-commit-push/loopPushCMDwarning.bat
+++ git-commit-push/loopPushCMDwarning.bat
@@ -1,13 +1,13 @@
+echo "setFor4hrs"
+
 @echo off
-set /p iterations="Enter the number of iterations: "
-set /p timeoutDuration="Enter the timeout duration (in seconds): "
-set /a count=0
+set timeoutDuration=30
+set /a endTime=%time:~0,2%+4
+if %endTime% GTR 23 set /a endTime-=24
 :loop
 call loopPush.bat
 timeout /t %timeoutDuration%
-set /a count+=1
-if %count%==%iterations% goto endloop
+if %time:~0,2% GEQ %endTime% goto endloop
 goto loop
 :endloop
-chcp 437
-start cmd /k "color 4F & echo ┌────────────────────────────────────────────┐ & echo │                                            │ & echo │ Warning: The script has ended.             │ & echo │ Please restart it if you want to continue  │ & echo │ monitoring the repository for changes.     │ & echo │                                            │ & echo └────────────────────────────────────────────┘"
+start cmd /k "color 4F & echo +--------------------------------------------+ & echo |                                            | & echo | Warning: The script has ended.             | & echo | Please restart it if you want to continue  | & echo | monitoring the repository for changes.     | & echo |                                            | & echo +--------------------------------------------+"
